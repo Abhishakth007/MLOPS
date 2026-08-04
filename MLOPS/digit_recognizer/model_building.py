625e63dee37f38bc9e3bc98887b22bc0ee48d9cd
Ubuntu <ubuntu@ip-172-31-30-213.ec2.internal>
2026-08-04 11:09:29 +0000

    Repo Fixed

--- MLOPS/digit_recognizer/model_building.py
+++
@@ -1,9 +0,0 @@
-from sklearn.linear_model import LogisticRegression
-
-
-def model_builder(x_train , y_train):
-	lr = LogisticRegression(max_iter = 1000)
-	model = lr.fit(x_train , y_train.values.ravel())
-	return model
-
-	
--- MLOPS/digit_recognizer/utils/csv_reader.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-path_to_file = "/home/ubuntu/MLOPS/digit_recognizer/Exports/raw.csv"
-
-data = pd.read_csv(path_to_file)
-df = pd.DataFrame(data)
-print(df.head)
